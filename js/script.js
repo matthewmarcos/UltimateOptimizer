0f600cfbaa0fb3e2324d1010bc27f66e70d513d9
Matthew Marcos <matthewmarcos94@gmail.com>
2016-11-29 12:07:25 +0800

    Create toast and can select food

--- js/script.js
+++ js/script.js
@@ -51,6 +51,40 @@ let rootVue = new Vue({
                 }
                 return x;
             });
+        },
+
+        addFood() {
+            const alreadyPicked = _.some(this.dietarySolver.picks, x => {
+                return x.food === this.dietarySolver.focusedFood.food;
+            });
+
+            if(!alreadyPicked) {
+                const food = this.dietarySolver.focusedFood.food;
+                // Toggle isAdded flag
+                this.dietarySolver.choices = this.dietarySolver.choices.map(x => {
+                    if(x.key === food) {
+                        x.isAdded = true;
+                    }
+                    return x;
+                });
+
+                // Add the food to the picks array
+                this.dietarySolver.picks.push(this.dietarySolver.focusedFood);
+
+                // Toast that the food has successfully been added
+                const msgString = 'Added ' +
+                    this.dietarySolver.focusedFood.food.replace(/_/g, ' ') +
+                    '.';
+                Materialize.toast(msgString, 2000);
+            }
+            else {
+                // Toast that this food item has already been added!
+                const msgString = 'Napili mo na ang ' +
+                    this.dietarySolver.focusedFood.food.replace(/_/g, ' ') +
+                    '.';
+
+                Materialize.toast(msgString, 2000);
+            }
         }
 
     }
